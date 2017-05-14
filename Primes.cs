d4266cb66dc3c77fea5a14d8ec89dacef024b3ec
Marco Labarile <labarilem@gmail.com>
2017-05-14 13:45:15 +0200

    vscode project added

---
+++ Primes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+namespace ProjectEuler.Math
+{
+    public static class Primes
+    {
+        public static bool IsPrime(int number)
+        {
+            return true;
+        }
+    }
+}
